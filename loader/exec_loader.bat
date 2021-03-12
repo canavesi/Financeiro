309086f79b9b6f44c4f91fecfd2d751ff9cd273a
canavesi <carlos.canavesi@uol.com.br>
2021-03-12 13:37:31 -0300

    final fase 2

--- loader/exec_loader.bat
+++ loader/exec_loader.bat
@@ -2,7 +2,7 @@ C:\ids10g\BIN\sqlplusw.exe /nolog @C:\Desenv\Oracle\Financeiro\loader\Abre_arq.s
 
 C:\oracle\product\11.2.0\dbhome_1\BIN\sqlldr userid=financeiro/financeiro@teste control=C:\Desenv\Oracle\Financeiro\loader\loader.ctl log=C:\Desenv\Oracle\Financeiro\loader\loader.log bad=C:\Desenv\Oracle\Financeiro\loader\loader.bad
 
---C:\ids10g\BIN\sqlplusw.exe /nolog @c:\desenv\fluxo\loader\visa\fecha_arq.sql
+C:\ids10g\BIN\sqlplusw.exe /nolog @C:\Desenv\Oracle\Financeiro\loader\fecha_arq.sql
 
 
 exit
